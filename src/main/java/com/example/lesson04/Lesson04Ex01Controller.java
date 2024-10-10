ce4da8dd1bb155d635177dc81f51977f388d5af1
IMYONGJIN\dksp5 <dksp56@naver.com>
2024-10-11 04:23:56 +0900

    10-11

--- src/main/java/com/example/lesson04/Lesson04Ex01Controller.java
+++ src/main/java/com/example/lesson04/Lesson04Ex01Controller.java
@@ -2,12 +2,15 @@ package com.example.lesson04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.lesson04.bo.UserBO;
+import com.example.lesson04.domain.User;
 
 @Controller // HTML 경로일 때는 @ResponseBody가 없다 
 @RequestMapping("/lesson04/ex01") // 공통 경로 
@@ -38,5 +41,25 @@ public class Lesson04Ex01Controller {
 			// 결과 화면 이동 
 			return "lesson04/afterAddUser";	
 	}
+	// 최신 가입자 정보 화면
+	// http://localhost/lesson04/ex01/latest-user-view
+	// 주소를 치고 들어오면 바로 보이기 때문에 GET Mapping
+	@GetMapping("/latest-user-view")
+	public String latestUserView(Model model) {
+		
+		// DB select (브레이크 포인트 걸어서 User를 잘 가져왔는지 확인)
+		User user = userBO.getLatestUser();
+		
+		// 컨트롤러가 Model이란 상자에 데이터(User)를 집어넣는다. 
+		// HTML(View)가 Model에서 꺼내서 쓴다.
+		
+		// key value로 넣음 
+		model.addAttribute("result", user);
+		model.addAttribute("title", "최근 가입된 유저");
+		
+		
+		return "lesson04/latestUser"; // view의 경로 
+	}
+	
 	
 }
--- src/main/java/com/example/lesson04/mapper/UserMapper.java
+++ src/main/java/com/example/lesson04/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.example.lesson04.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.example.lesson04.domain.User;
+
 @Mapper
 public interface UserMapper {
 
@@ -15,5 +17,9 @@ public interface UserMapper {
 			@Param("email") String email,
 			@Param("introduce") String introduce
 			); 
+	
+	// input X 쿼리문 혼자 알아서 하기때문
+	// return : User (하나의 행만 가져오기 때문에 User 하나만 가져옴) 단건이면 없을 수 있다. 
+	public User selectLatestUser();
 	
 }
